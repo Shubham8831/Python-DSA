bb41f0dc43b938f2a817421c1ea2ce1edb401050
Shubham8831 <shubuamsingh2002@gmail.com>
2025-02-13 04:05:06 +0530

    introduction for dsa with python

--- 1_introduction.py
+++ 1_introduction.py
@@ -5,8 +5,30 @@
     - able to solve complex problem
     - placements
 
-#2  
+#2 importance of structuring data?
+    - dictionary
+    - map
+    - ledger(bahi khata)
 
+#3 what is data structure?
+    - way of storing and organizing data in a computer so that it can be used efficiently
 
+#4 where are datastructure resides?
+    - arrangement of variables in RAM
+
+#5 classification of data structure?
+    - linear [array, dynamic array, queue, stack, linkedlist, deque ]
+    - non linear [BST, AVL, B-Tree, B+ Tree, Graph]
+
+#6 Algorithm?
+    - step by step linguistic representaion of logic to solve a given problem.
+
+#7 Prerequisites?
+    - built-in types
+    - functions
+    - control statement
+    - class and objects
+    - __init__()
+    - types of variables and function in class
 
 '''
