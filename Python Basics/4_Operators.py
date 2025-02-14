e3b198050675bdc75f31671d3fc70f2075c2dbeb
Shubham8831 <shubuamsingh2002@gmail.com>
2025-02-14 20:58:02 +0530

    basic python operators

---
+++ Python Basics/4_Operators.py
@@ -0,0 +1,32 @@
+#1 Arithemetic Operator 
+a = 10
+b =5
+print(a+b) # add
+print(a-b) #sub
+print(a*b) #mul
+print(a/b) #div
+print(a//b) #floor div -> gives only integer value
+print(a%b) #modulus -> remainder
+print(a**b) # a^b -> exponential
+
+
+
+#2 Comparision Operator
+s1 = 'shub'; s2 = 'SHUB'
+n1 = 45; n2 = 55
+print(s1 == s2) # equal to 
+print(s1 != s2) # not equal to 
+print(n1 > n2) # greater then
+print(n1 >= n2) # greater then equal to 
+print(n1 < n2) # less then
+print(n1 <= n2) # less then equal to 
+
+
+
+#3 Logican Operator : And, Not, OR
+x = True
+y = True
+print(x and y) # both true then true
+print(x or y) # any one true then true
+print(not x) # opposite of x
+
